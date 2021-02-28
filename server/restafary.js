06724dee7f859dc16623008aed322b888d5082af
coderaiser <coderaiser@cloudcmd.io>
2021-02-28 11:05:43 +0200

    feature(package) redzip v2.1.0

--- server/restafary.js
+++ server/restafary.js
@@ -204,9 +204,7 @@ async function onFS(params, callback) {
         });
         
         p.response.setHeader('Content-Type', contentType);
-        
-        if (contentLength)
-            p.response.setHeader('Content-Length', contentLength);
+        p.response.setHeader('Content-Length', contentLength);
         
         if (method === 'HEAD')
             return p.response.end();
